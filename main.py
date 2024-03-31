29df580324a8dc085af82d9de99b3d1b72ec4e15
copibra20234567 <130291138+copibra20234567@users.noreply.github.com>
2024-03-31 15:07:33 +0300

    sayt

--- main.py
+++ main.py
@@ -7,4 +7,25 @@ def index():
 
     return render_template("index.html")
 
+@app.route("/notebooks")
+def notebooks_page():
+    return render_template("notebooks.html")
+
+@app.route("/Golovna")
+def Golovna_page():
+    return render_template("Golovna.html")
+
+@app.route("/Computer")
+def Computer_page():
+    return render_template("Computer.html")
+
+@app.route("/Mouse")
+def Mouse_page():
+    return render_template("Mouse.html")
+
+@app.route("/Headphones")
+def Headphones_page():
+    return render_template("Headphones.html")
+
+
 app.run(debug=True)
